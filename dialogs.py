03e0d2bd82a43a20838f22c0355c634c20bb9769
Kaptollika3d <119661884+Kaptollika3d@users.noreply.github.com>
2022-12-09 17:31:21 +0500

    multi_select

--- dialogs.py
+++ dialogs.py
@@ -5,12 +5,12 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.types import CallbackQuery
 
-from aiogram_dialog import DialogManager, DialogRegistry, Window, Dialog, StartMode
-from aiogram_dialog.widgets.kbd import Select, Button, Column, Multiselect, Next
+from aiogram_dialog import DialogManager, DialogRegistry, Window, Dialog, StartMode, ChatEvent
+from aiogram_dialog.widgets.kbd import Select, Button, Column, Multiselect, Next, Back, ManagedMultiSelectAdapter
 from aiogram_dialog.widgets.text import Format, Const
+from aiogram_dialog.widgets.input import MessageInput
 
-import getinfo
-
+import getdata
 
 class MySG(StatesGroup):    
     select_timetable = State()
@@ -23,26 +23,29 @@ main_window = Window(
     state=MySG.main,
 )
 
+async def on_clients_by_time_selected(c: CallbackQuery, multi:ManagedMultiSelectAdapter, widget: Any, dialog_manager: DialogManager):
+    #print("Clients selected: ", dialog_manager.current_context().dialog_data.get("clients"))  
+    print(multi.get_checked())
+    #dialog_manager.current_context().dialog_data["s_times"] = item_id
+    #await dialog_manager.start(MySG.multiselect_clients_by_time, mode=StartMode.RESET_STACK)
+    #await dialog.next(dialog_manager)
+
 clients_by_time_select = Multiselect(
     Format("✓ {item[1]}"),
     Format("{item[1]}"),
     id="m_clients_by_time",
     item_id_getter=operator.itemgetter(0),
     items="clients",
+    on_click=on_clients_by_time_selected,
 )
-
-async def on_clients_by_time_selected(c: CallbackQuery, widget: Any, dialog_manager: DialogManager):
-    print("Clients selected: ", dialog_manager.current_context().dialog_data.get("clients"))  
-    #dialog_manager.current_context().dialog_data["s_times"] = item_id
-    #await dialog_manager.start(MySG.multiselect_clients_by_time, mode=StartMode.RESET_STACK)
-    #await dialog.next(dialog_manager)
 
 clients_by_time_select_window = Window(
     Format("Выберите посетителей:"),
     Column(clients_by_time_select),
-    Button(Const("ОК"), id="ok", on_click=on_clients_by_time_selected),
+    Button(Const("ОК"), id="ok"),# on_click=on_clients_by_time_selected),
+    Back(Const('Назад')),
     state=MySG.multiselect_clients_by_time,
-    getter=getinfo.get_clients_by_time,
+    getter=getdata.get_clients_by_time,
     preview_add_transitions=[Next()],
 )
 
@@ -64,7 +67,7 @@ timetable_select_window =  Window(
     Format("Выберите время:"), 
     Column(timetable_select), 
     state=MySG.select_timetable, 
-    getter=getinfo.get_timetable,
+    getter=getdata.get_timetable,
 )
 
 dialog = Dialog(
